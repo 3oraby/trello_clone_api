30854c35f23df9c12164486f1ca9f78e8399acc8
3oraby <mohamedahmed21102003@gmail.com>
2026-01-18 20:33:13 +0200

    prepared app for deploy

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
@@ -13,15 +23,52 @@ const checklistItemRoutes = require("./routes/checklistItemRoutes.js");
 
 const path = require("path");
 
+app.enable("trust proxy");
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "templates"));
+
+app.use(cors());
+app.options(/.*/, cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(helmet());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [],
+  }),
+);
+
+app.use(compression());
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use(helmet());
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.requestTime);
   next();
 });
 
